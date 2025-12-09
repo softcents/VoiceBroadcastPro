358795a2413fe570f4d0c5430a2cb2a5165161d2
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2025-12-09 16:39:23 +0600

    Update navigation icon in AudioResource to use Tabler music icon

--- app/Filament/User/Resources/Audio/AudioResource.php
+++ app/Filament/User/Resources/Audio/AudioResource.php
@@ -15,14 +15,14 @@ use App\Models\Audio;
 use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use LaraZeus\Tabler\Tabler;
 
 final class AudioResource extends Resource
 {
     protected static ?string $model = Audio::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Tabler::Music;
 
     protected static ?int $navigationSort = 30;
 
